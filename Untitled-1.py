f56aa7ed270d39728b6e64f54fbd38937f3bc682
Sara Törnström <saratornstrom@MacBook-Air-som-tillhor-Sara.local>
2021-09-13 14:13:13 +0200

    cleanup

--- Untitled-1.py
+++
@@ -1,7 +0,0 @@
-savings = 100
-growth_multiplier = 1.1
-desc = "compound interest"
-# Assign product of growth_multiplier and savings to year1
-year1 = savings * growth_multiplier
-# Print the type of year1
-print(type(year1))
--- x.py
+++
@@ -1,7 +0,0 @@
-variabel1="Hej"
-variabel2=4
-variabel3=4.3
-
-print(type(variabel1))
-print(type(variabel2))
-print(type(variabel3))
